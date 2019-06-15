6a768522c7f55a277ed0c55e8e561da33ec62555
helloryantg <helloryantg@gmail.com>
2019-06-15 09:43:21 -0700

    Adds create react app material

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/dev', (req, res) => {
 app.get('*', (req, res) => {
     res.send('404 not found!')
 })
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Dev app listening on port ${port}`)
